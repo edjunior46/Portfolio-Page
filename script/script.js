9c589eaf5347aba5f6e3e6d5f89a3985b7e4816a
Ederson Junior <ed.esmeraldino7@gmail.com>
2023-04-19 20:16:19 -0300

    style changes

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 let cards = document.querySelectorAll('.cardFront');
 
 for (let i = 0; i < cards.length; i++) {
-    cards[i].style.backgroundImage = 'url(/style/img/project0' + (i+1) + '.png)';
+    cards[i].style.backgroundImage = 'url(style/img/project0' + (i+1) + '.png)';
     cards[i].style.backgroundSize = 'cover';
     cards[i].style.backgroundPosition = 'center';
     cards[i].style.backgroundRepeat = 'no-repeat'; 
 }
 
-cards[cards.length-1].style.backgroundImage = 'url(/style/img/github.png)';
+cards[cards.length-1].style.backgroundImage = 'url(style/img/github.png)';
 cards[cards.length-1].style.backgroundSize = '50%';
 
